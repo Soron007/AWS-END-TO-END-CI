8b5e5e50002e1f78df08384096037692a3f513f1
Souvik Mitra <87466601+Soron007@users.noreply.github.com>
2025-01-11 21:43:32 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
+sudo su - root
 # Pull the Docker image from Docker Hub
-docker push souvik0007/simple-python-flask-app:latest
+docker pull souvik0007/simple-python-flask-app:latest
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 souvik0007/simple-python-flask-app:latest
